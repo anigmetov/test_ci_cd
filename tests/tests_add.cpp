9829169f9e0bbfcf3399b5fafeb00d0b11cf0c90
Arnur Nigmetov <anigmetov@lbl.gov>
2022-02-20 21:51:53 -0800

    Fix tests back

--- tests/tests_add.cpp
+++ tests/tests_add.cpp
@@ -6,6 +6,6 @@
 TEST_CASE("Basic test")
 {
     REQUIRE(add<int>(2, 2) == 4);
-    REQUIRE(add<int>(3, 2) == 6);
+    REQUIRE(add<int>(3, 2) == 5);
     REQUIRE(add<int>(std::numeric_limits<int>::max(), 0) == std::numeric_limits<int>::max());
 }
